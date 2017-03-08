6a0d6a900aff4509a157b227a48b67c7c5ab0f2c
willxcience <wilwu@ucdavis.edu>
2017-03-07 23:17:36 -0800

    alpha

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include "src/darknet.c"
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    //test_resize("data/bad.jpg");
+    //test_box();
+    //test_convolutional_layer();
+    if(argc < 2){
+        fprintf(stderr, "usage: %s <function>\n", argv[0]);
+        return 0;
+    }
+    gpu_index = find_int_arg(argc, argv, "-i", 0);
+    if(find_arg(argc, argv, "-nogpu")) {
+        gpu_index = -1;
+    }
+
+#ifndef GPU
+    gpu_index = -1;
+#else
+    if(gpu_index >= 0){
+        cuda_set_device(gpu_index);
+    }
+#endif
+
+	return 0;
+}
+
